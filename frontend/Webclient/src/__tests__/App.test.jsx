0353f67951d35b6c1c218caf136c70abb0a11b72
Sadat Akhtar <sadatakhtar@hotmail.co.uk>
2025-09-14 13:59:40 +0100

    Removed tests to check sonar cloud detection

--- frontend/Webclient/src/__tests__/App.test.jsx
+++ frontend/Webclient/src/__tests__/App.test.jsx
@@ -3,12 +3,12 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('App Component', () => {
-  test('renders title without error', () => {
-    render(<App />);
-    expect(screen.getByText(/Vehicle Shipping Management App/i)).toBeInTheDocument();
-  });
-  test('renders subtitle', () => {
-    render(<App />);
-    expect(screen.getByText(/Vehicle list/i)).toBeInTheDocument();
-  })
+//   test('renders title without error', () => {
+//     render(<App />);
+//     expect(screen.getByText(/Vehicle Shipping Management App/i)).toBeInTheDocument();
+//   });
+//   test('renders subtitle', () => {
+//     render(<App />);
+//     expect(screen.getByText(/Vehicle list/i)).toBeInTheDocument();
+//   })
 });
