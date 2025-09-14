6e59ec6d861cd2e3090f6b28910f8982b6221f29
Sadat Akhtar <sadatakhtar@hotmail.co.uk>
2025-09-14 12:44:54 +0100

    Configured jest testing, and replaced yaml file to only check FE in sonarcloud

---
+++ frontend/Webclient/jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(svg|jpg|jpeg|png|gif)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+};
---
+++ frontend/Webclient/src/__tests__/App.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('App Component', () => {
+  test('renders title without error', () => {
+    render(<App />);
+    expect(screen.getByText(/Vehicle Shipping Management App/i)).toBeInTheDocument();
+  });
+  test('renders subtitle', () => {
+    render(<App />);
+    expect(screen.getByText(/Vehicle list/i)).toBeInTheDocument();
+  })
+});
